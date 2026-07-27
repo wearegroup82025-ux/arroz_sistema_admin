fc4a9e0a34e139a87bf133d53e3d17675e2ef289
Arroz Sistema <wearegroup82025@gmail.com>
2026-07-27 16:37:35 +0800

    cart

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     // START: FlutterFire Configuration
     id("com.google.gms.google-services")
+    id("org.jetbrains.kotlin.android")
     // END: FlutterFire Configuration
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
